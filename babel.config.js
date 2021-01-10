6cab929380316425e154af4d6e49f7179b64523f
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2021-01-10 15:36:31 +0900

    env: remove all

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,24 @@
-const presets = ['@babel/preset-react'];
-const plugins = [
-  '@babel/plugin-transform-template-literals',
-  '@babel/plugin-transform-arrow-functions',
-];
+// const presets = [
+//   '@babel/preset-react',
+  // '@babel/preset-env',
+//   {
+//     targets: {
+//       // 크롬
+//       chrome: '40',
+//     },
+//     // entry: 지원하는 브라우저에서 필요한 폴리필만 포함시킨다. 
+//     // usage: 코드에서 사용된 기능의 폴리필만 포함시킨다.
+//     useBuiltIns: 'usage',
+//     // 바벨에게 core-js 버전을 알려준다.
+//     corejs: { version: 3, proposals: true },
+//   },
+// ];
+// const plugins = [
+//   '@babel/plugin-transform-template-literals',
+//   {
+//     loose: true,
+//   },
+//   '@babel/plugin-transform-arrow-functions',
+// ];
 
-module.exports = { presets, plugins };
+// module.exports = { presets, plugins };
