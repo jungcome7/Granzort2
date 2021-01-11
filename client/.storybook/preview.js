2392c51719b059d8717652f57608edf574ff351c
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2021-01-12 01:58:14 +0900

    feat : add decorators to the preview of Storybook

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,4 +1,16 @@
+import React from 'react';
+import GlobalStyle from '../src/styles/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
--- client/src/components/common/Button/Button.tsx
+++ client/src/components/common/Button/Button.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as S from './ButtonStyle';
 
-type ButtonProps = {
+export type ButtonProps = {
   children?: string;
 };
 
