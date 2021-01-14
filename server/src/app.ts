d0f768f9deed4f78e333f4fd9bfc09e990ba9243
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2021-01-14 22:28:10 +0900

    feat : add a mysql connection pool

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,12 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import morgan from 'morgan';
 dotenv.config();
 
 const app = express();
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
