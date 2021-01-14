202ab675136bbbd575c9e2d980ceedb51f47bade
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2021-01-14 22:05:37 +0900

    feat : create server

---
+++ server/src/app.ts
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+import express from 'express';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
