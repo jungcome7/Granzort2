a9454a26cb812c89624e84c264741cc001c00d2b
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2021-01-16 14:03:09 +0900

    feat : add a redux store

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,10 +2,26 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer, { rootSaga } from './modules';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
+);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
+  <Provider store={store}>
     <BrowserRouter>
       <App />
-    </BrowserRouter>,
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
--- client/src/pages/SignIn/SignIn.tsx
+++ client/src/pages/SignIn/SignIn.tsx
@@ -5,16 +5,6 @@ import { Header } from '../../components/Header';
 import * as S from './SignInStyle';
 
 const SignIn = () => {
-  // API 테스트용
-  const [data, setData] = useState();
-  useEffect(() => {
-    const signUpUser = async () => {
-      const res = await API.User.signUp({ userId: 'id2', password: 'pass2' });
-      setData(res);
-    };
-    signUpUser();
-    console.log(data);
-  });
   return (
     <S.Container>
       <Header />
